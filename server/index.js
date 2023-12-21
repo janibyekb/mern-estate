d7be4fb1c2041f05fbcb9e675363407d62a85a29
bolatkha <janibyek.bolatkhan@nokia.com>
2023-12-21 22:57:13 +0100

    user model added

--- server/index.js
+++ server/index.js
@@ -1,4 +1,18 @@
+"use strict";
+import dotenv from "dotenv";
+dotenv.config();
+
 import express from "express";
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
 
